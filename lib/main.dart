0135c43288e76800bb66bfe3cafd17aec240d768
Jean Carlo <jeancarlo@lamediadl.com>
2020-10-14 08:44:56 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+
+import 'package:fcm_flutter/widget/messaging_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String appTitle = 'Firebase messaging';
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: appTitle,
+        home: MainPage(appTitle: appTitle),
+      );
+}
+
+class MainPage extends StatelessWidget {
+  final String appTitle;
+
+  const MainPage({this.appTitle});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MessagingWidget(),
+      );
+}
